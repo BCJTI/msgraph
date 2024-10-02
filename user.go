59a368a3678e8effc4ac02eeb6bd6d8c8335818e
Eduardo <jacocasa@gmail.com>
2024-10-02 16:29:18 -0300

    fix GetUserInfo

--- user.go
+++ user.go
@@ -3,21 +3,21 @@ package msgraph
 import "fmt"
 
 // GetUserInfo sends an email using Microsoft Graph API
-func (c *Client) GetUserInfo() error {
+func (c *Client) GetUserInfo() (*UserInfo, error) {
 	if c.Token == nil {
-		return fmt.Errorf("missing access token. Please obtain one first")
+		return nil, fmt.Errorf("missing access token. Please obtain one first")
 	}
 
 	if !c.Token.Valid() {
 		err := c.OAuthRefreshToken()
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	userInfo := UserInfo{}
+	userInfo := &UserInfo{}
 
-	err := c.Get("/me", nil, nil, &userInfo)
+	err := c.Get("/me", nil, nil, userInfo)
 
-	return err
+	return userInfo, err
 }
